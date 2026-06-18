116b4dc0f64864efb023afd8b8148aae9614400e
Urs Ziegler <ziegler.urs@gmail.com>
2026-06-19 01:21:24 +0200

    v1.0.12 - towards the ios, android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -20,7 +21,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 31
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
