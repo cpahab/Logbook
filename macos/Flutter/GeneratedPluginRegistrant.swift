ee41776b3478f19b47dd105347d6ee30927064cf
Urs Ziegler <ziegler.urs@gmail.com>
2026-06-26 15:11:16 +0200

    chore: update macOS Podfile.lock to Firebase SDK 12.15.0
    Regenerated after flutter pub add firebase_app_check bumped the Firebase
    SDK constraint from 12.14.0 to 12.15.0. Also updates generated plugin
    registrant files for macOS and Windows.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import cloud_firestore
 import connectivity_plus
 import file_picker
+import firebase_app_check
 import firebase_auth
 import firebase_core
 import firebase_storage
@@ -24,6 +25,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
